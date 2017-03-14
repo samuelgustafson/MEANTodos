1f9b7b89b11d0974427b84aa44e2c008decf42ef
sam gustafson <samelgw@gmail.com>
2017-03-14 19:11:35 -0400

    completed the post route for todos and and put/delete routes for todos by id, everythings broke

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ var todoRouter = require('./routers/todo.router');
 
 var port = process.env.PORT || 8080;
 var mongoURI = process.env.MONGOURI || require('./secrets').mongoURI;
-
+//power-up middleware
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
-
+//connect to database
 mongoose.connect(mongoURI);
+
+server.use(todoRouter);
+
 server.listen(port, function(){
   console.log('Now listening on port...', port);
 })
--- routers/todo.router.js
+++ routers/todo.router.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var todoRouter = express.Router();
 var Todo = ('../models/todo.model');
-
 // GET /todos
-todoRouter.get('/todos', fuction(req, res){
+todoRouter.get('/todos', function(req, res){
   Todo.find({}, function(err, documents){
     if(err){
       res.status(500).json({
@@ -31,5 +30,47 @@ todoRouter.get('/todos/:id', function(req, res){
   });
 });
 // POST /todos/
+todoRouter.post('/todos', function(req, res){
+  var todo = new Todo(req.body);
+  todo.save(function(err, document){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else{
+      res.status(201).json({
+        msg: 'Success'
+      });
+    }
+  });
+});
 // PUT /todos/:id
+todoRouter.put('/todos/:id', function(req, res){
+  Todo.findOneAndUpdate({_id: req.params.id}, req.body, function(err, document){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully updated'
+      });
+    }
+  });
+});
 // DELETE /todos/:id
+todoRouter.delete('/todos/:id', function(req, res){
+  Todo.remove({_id: req.params.id}, req.body, function(err, document){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        msg: 'Successfully Deleted'
+      });
+    }
+  });
+});
+
+module.exports = todoRouter;
